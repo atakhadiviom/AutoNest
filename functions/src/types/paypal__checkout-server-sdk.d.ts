0a47c6f499917180dcbd501992ccf5c86fe12fae
ATA KHADIVI <atakhadiviom@gmail.com>
2025-05-22 11:38:34 +0000

    studio-4948799546:~/studio/functions{master}$ firebase deploy --only functions
    === Deploying to 'autonest-vn4w5'...
    
    i  deploying functions
    Running command: npm --prefix "$RESOURCE_DIR" run lint
    
    > lint
    > eslint --ext .js,.ts .
    
    /home/user/studio/functions/src/index.ts
       70:17  warning  Unexpected any. Specify a different type  @typescript-eslint/no-explicit-any
      125:25  warning  Unexpected any. Specify a different type  @typescript-eslint/no-explicit-any
      150:17  warning  Unexpected any. Specify a different type  @typescript-eslint/no-explicit-any
    
    /home/user/studio/functions/src/types/paypal__checkout-server-sdk.d.ts
      2:1  error  This line has a length of 86. Maximum allowed is 80  max-len
    
    ✖ 4 problems (1 error, 3 warnings)
    
    Error: functions predeploy error: Command terminated with non-zero exit code 1

--- functions/src/types/paypal__checkout-server-sdk.d.ts
+++ functions/src/types/paypal__checkout-server-sdk.d.ts
@@ -1,5 +1,5 @@
 // This file provides a basic module declaration for @paypal/checkout-server-sdk
-// to satisfy the TypeScript compiler, as the package does not ship with its own types
+// to satisfy the TS compiler, as the package doesn't ship its own types
 // and no @types/paypal__checkout-server-sdk package exists.
 
 declare module "@paypal/checkout-server-sdk";
