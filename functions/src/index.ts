a4324092bc5652fae41434ae42989bd736b68f01
ATA KHADIVI <atakhadiviom@gmail.com>
2025-05-22 15:45:03 +0000

    there is no deployment for function deploying I need to run firebase init?

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,13 +3,15 @@
  */
 import * as functions from "firebase-functions";
 
-// Basic HTTP function for testing deployment.
+// Basic HTTP function for testing deployment (paypalAPI)
+// This is extremely simplified to help diagnose deployment issues.
 export const paypalAPI = functions.https.onRequest((request, response) => {
   functions.logger.info("[paypalAPI] Basic test endpoint called!");
   response.send("Hello from paypalAPI basic test!");
 });
 
 // Standard Hello World function for testing deployment.
+// This is extremely simplified.
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("[helloWorld] Endpoint called!");
   response.send("Hello from Firebase (helloWorld)!");
