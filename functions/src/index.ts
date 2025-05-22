743398945a3344a2dff7b4d4d418e0497dbf364d
ATA KHADIVI <atakhadiviom@gmail.com>
2025-05-22 12:52:23 +0000

    studio-4948799546:~/studio/functions{master}$ npm run lint
    > lint
    > eslint --ext .js,.ts .
    
    /home/user/studio/functions/src/index.ts
      10:1  error  This line has a length of 82. Maximum allowed is 80  max-len
    
    ✖ 1 problem (1 error, 0 warnings)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,31 +3,13 @@
  *
  * This file defines HTTP-triggered functions.
  * The paypalAPI function is currently a placeholder.
- * For production, it would handle PayPal REST API calls securely.
- * Ensure Firebase project billing is active for deployment.
+ * If this deploys, the issue is not project/env config
+ * for basic function creation.
  */
 import * as functions from "firebase-functions";
-// import * as admin from "firebase-admin"; // Not used in this simplified version
-// import express from "express"; // Not used in this simplified version
-// import cors from "cors"; // Not used in this simplified version
-// import bodyParser from "body-parser"; // Not used in this simplified version
-// import paypalClient from "./paypalClient"; // Not used
-// import paypal from "@paypal/checkout-server-sdk"; // Not used
-
-// Initialize Firebase Admin SDK if not already initialized.
-// This uses Application Default Credentials when deployed.
-// if (admin.apps.length === 0) {
-//   admin.initializeApp();
-// }
-// const db = admin.firestore();
-
-// const app = express();
-// app.use(cors({origin: true}));
-// app.use(bodyParser.json());
 
 // Simplified paypalAPI for testing deployment.
-// If this deploys, the issue is not project/env config
-// for basic function creation.
+// If this deploys, issue is not project/env config for function creation.
 export const paypalAPI = functions.https.onRequest((request, response) => {
   functions.logger.info("Simplified paypalAPI called!", {structuredData: true});
   response.send("Simplified paypalAPI response!");
